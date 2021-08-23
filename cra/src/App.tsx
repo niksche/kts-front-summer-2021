a0be73894a8b8deb48021eaf6fdba4d9cc5d4afa
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-08-23 09:56:49 +0300

    include ts

--- cra/src/App.tsx
+++ cra/src/App.tsx
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const sayHello: string = 'hello!';
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          heheeh {sayHello}
         </a>
       </header>
     </div>
