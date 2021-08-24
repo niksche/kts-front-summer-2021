9451a9196c0060d8745a7e8bbf317c0401a4e976
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-08-24 13:02:05 +0300

    trying to implement

--- cra/src/store/GitHubStore/GitHubStore.ts
+++ cra/src/store/GitHubStore/GitHubStore.ts
@@ -1,13 +1,23 @@
 import ApiStore from '../../shared/store/ApiStore';
 import {IGitHubStore} from "./types";
+import RequestParams from '../../shared/store/types';
 
 export default class GitHubStore implements IGitHubStore {
-    private readonly apiStore = new ApiStore(); // TODO: не забудьте передать baseUrl в конструктор
+    private readonly apiStore = new ApiStore("/"); // TODO: не забудьте передать baseUrl в конструктор
+
+
 
     // TODO: реализовать интерфейс IGitHubStore
 
-    async getOrganizationReposList(params: GetOrganizationReposListParams): Promise<ApiResp<RepoItem[]>> {
+
+    
+    async getOrganizationReposList(params: RequestParams<string>): Promise<ApiResp<RepoItem[]>> {
+        return this.apiStore.request(params).then((res) => {
+            return res;
+        })
         // TODO: Здесь сделайте вызов из this.apiStore и верните результат
         // Документация github: https://docs.github.com/en/rest/reference/repos#list-organization-repositories
     }
 }
+
+
