b345977d23ab1036253c2a42e1a7135c05bb8bf8
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-08-25 15:42:08 +0300

    fix githubstoreclasss

--- cra/src/shared/store/ApiStore/types.ts
+++ cra/src/shared/store/ApiStore/types.ts
@@ -42,7 +42,7 @@ export type ApiResponse<SuccessT, ErrorT> =
 }
     | {
     success: false;
-    data: null;
+    data: any;
     status: StatusHTTP;
 };
 
--- cra/src/store/GitHubStore/GitHubStore.ts
+++ cra/src/store/GitHubStore/GitHubStore.ts
@@ -1,14 +1,19 @@
 import ApiStore from '../../shared/store/ApiStore';
-import { RequestParams } from '../../shared/store/ApiStore/types';
+import { HTTPMethod, RequestParams } from '../../shared/store/ApiStore/types';
 import {IGitHubStore} from "./types";
 // import RequestParams from '../../shared/store/types'
 
 
 export default class GitHubStore implements IGitHubStore {
-    private readonly apiStore = new ApiStore("https://api.github.com/orgs/google/repos"); // TODO: не забудьте передать baseUrl в конструктор
+    private readonly apiStore = new ApiStore(`https://api.github.com/`); // TODO: не забудьте передать baseUrl в конструктор
 
-    async getOrganizationReposList(params: RequestParams<string>): Promise<ApiResponse<RepoItem[]>> {
-        let req = await this.apiStore.request(params);
+    async getOrganizationReposList(organizationName: string): Promise<ApiResponse<RepoItem[]>> {
+        let req = await this.apiStore.request({
+            method: HTTPMethod.GET,
+            endpoint: `orgs/${organizationName}/repos`,
+            headers: "",
+            data: "",
+        });
         return req.data;
         
         // TODO: Здесь сделайте вызов из this.apiStore и верните результат
