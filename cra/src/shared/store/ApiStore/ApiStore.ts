4a0cdb4fa513bd20097b48053e91b6269169847a
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-08-25 14:33:39 +0300

    added some code

--- cra/src/shared/store/ApiStore/ApiStore.ts
+++ cra/src/shared/store/ApiStore/ApiStore.ts
@@ -1,4 +1,4 @@
-import {ApiResponse, IApiStore, RequestParams, StatusHTTP} from "./types";
+import {ApiResponse, HTTPMethod, IApiStore, RequestParams, StatusHTTP} from "./types";
 
 export default class ApiStore implements IApiStore {
     readonly baseUrl: string;
@@ -6,13 +6,45 @@ export default class ApiStore implements IApiStore {
         this.baseUrl = baseUrl;
     }
 
-    request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>> {  
-        return fetch(params.endpoint, {
+    async request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>> {  
+        let queryParam = '';
+        if (params.method === HTTPMethod.GET) {
+            queryParam = qs.stringify(params.data);
+        }
+
+        let data = '';
+        if (params.method === HTTPMethod.POST) {
+            data = JSON.stringify(params.data);
+        }
+
+        let endpoint : string = `${this.baseUrl}${params.endpoint}${queryParam}`;
+
+        let shit : Record<string, string> {
+            "Content-Type" : "application/json"
+            ...params.headers,
+        }
+        let response = await fetch(endpoint, {
             method: params.method,
             headers: params.headers,
-            body: JSON.stringify(params.data)
-        }).then((res) => {
-            return res.json()
+            body: data
         })
+
+        if (response.ok) {
+            let answ = await response.json();
+
+            return {
+                success:true,
+                data: answ,
+                status: response.status,
+            }
+        }
+
+        if (response.status != 200) {
+            return {
+                success: false,
+                data: '',
+                status: response.status,
+            }
+        }
     }
 }
