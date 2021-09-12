8b6056e1a30a46080d8fc55bbbe022d501c3d759
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-09-12 20:57:55 +0300

    added base routing to repos

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,18 @@
+import Button from '@components/Button';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
 import ReposSearchPage from "../ReposSearchPage";
 import "./App.css";
 const App = () => {
     return (
-        <ReposSearchPage />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/repos" component={ReposSearchPage} />
+                <Route>
+                    <Redirect to="/repos" />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 };
 
